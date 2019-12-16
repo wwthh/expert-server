d2a66b7c986d17f25ccc739d4030d05d2dfdfdf0
linan1109 <linan119@yeah.net>
2019-12-16 22:19:06 +0800

    debug id 问题

--- src/main/java/com/wwt/expertservice/controller/ExpertController.java
+++ src/main/java/com/wwt/expertservice/controller/ExpertController.java
@@ -19,8 +19,9 @@ public class ExpertController {
     private ExpertRepository expertRepository;
     public static String getUUID(){
         String s = UUID.randomUUID().toString();
+        s = s.substring(0,8)+s.substring(9,13)+s.substring(14,18)+s.substring(19,23)+s.substring(24);
         //去掉“-”符号
-        return s.substring(0,8)+s.substring(9,13)+s.substring(14,18)+s.substring(19,23)+s.substring(24);
+        return s.substring(0,24);
     }
     @PostMapping("/experts")
     public Map<String, Object> insertExpert(@RequestBody String requestbody) {
